5f7ae8bb645b31778a7aff01b63ec074420c4be4
conni <c.buschka@arcusx.com>
2018-08-15 19:54:40 +0200

    Added entrypoint with signal trap.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set +x
+pid="0"
+
+handle_signal() {
+  echo "Signal!"
+  if [ "x${pid}" != "x0" ]; then
+    kill -SIGTERM "${pid}"
+    wait "${pid}"
+  fi
+  exit 0
+}
+
+trap 'handle_signal' SIGINT SIGTERM SIGHUP SIGUSR1 SIGUSR2
+
+"$@" &
+pid="${!}"
+wait "${pid}"
+
